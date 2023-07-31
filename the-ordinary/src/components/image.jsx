d600ee10963afc8e0aac1f9ac79c97662a0f3a3c
Judeadeniji <judeadeniji66@gmail.com>
2023-07-31 19:41:42 +0100

    minor fix

--- the-ordinary/src/components/image.jsx
+++ the-ordinary/src/components/image.jsx
@@ -2,32 +2,31 @@ import { Component as box } from "@mejor";
 import http from "@app/services/http-client";
 
 function showImage(target, src, alt) {
-  const netStat = navigator.onLine;
-  if (src.includes("http") && !netStat) return;
-
-  if (target.src.length < 1 || target.src !== src) {
-    const cachedImage = sessionStorage.getItem(src);
-    if (cachedImage) {
-      target.src = cachedImage;
-      target.alt = alt;
-    } else {
-      http.get(src)
-        .then((response) => response.blob())
-        .then((blob) => {
-          const reader = new FileReader();
-          reader.onloadend = () => {
-            const base64data = reader.result;
-            sessionStorage.setItem(src, base64data);
-            target.src = base64data;
-            target.alt = alt;
-          };
-          reader.readAsDataURL(blob);
-        })
-        .catch((error) => {
-          console.error("Error fetching or caching image:", error);
-        });
+  try {
+    const netStat = navigator.onLine;
+    if (src.includes("example") || src.includes("http") && !netStat) return;
+  
+    if (target.src.length < 1 || target.src !== src) {
+      const cachedImage = sessionStorage.getItem(alt);
+      if (cachedImage) {
+        target.src = cachedImage;
+        target.alt = alt;
+      } else {
+        http.get(src)
+          .then((response) => response.blob())
+          .then((blob) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+              const base64data = reader.result;
+              sessionStorage.setItem(alt, base64data);
+              target.src = base64data;
+              target.alt = alt;
+            };
+            reader.readAsDataURL(blob);
+          })
+      }
     }
-  }
+  } catch (e) {}
 }
 
 export default box(({ 
