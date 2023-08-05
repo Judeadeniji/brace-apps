81a4f6d38bdf6e188c6fb4feef2121f65a0ec291
Judeadeniji <judeadeniji66@gmail.com>
2023-08-05 04:48:10 +0100

    added build folder

---
+++ test-app/www/assets/js/707.25fa5214.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkBraceJs=self.webpackChunkBraceJs||[]).push([[707],{707:(s,a,e)=>{e.r(a),e.d(a,{DELETE:()=>T,GET:()=>c,PATCH:()=>E,POST:()=>r,PUT:()=>n});var t=e(3278);const c=s=>{},r=async s=>{const a=await s.formData();t.store.dispatch({type:"SET",value:Object.fromEntries(a)})},n=s=>{},E=s=>{},T=s=>{}}}]);
